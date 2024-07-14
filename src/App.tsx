1d85d3f4a931d67e5b6f639758a106b1aeacf561
Alvin <alvinngow21@gmail.com>
2024-07-14 15:50:26 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Masthead from './components/common/Masthead';
+import SearchBar from './components/search/SearchBar';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {SearchResp} from './queries/getSearchResults';
+import SearchResOptions from './components/search/SearchResOptions';
+
+const queryClient = new QueryClient();
 
 function App() {
+  const [searchRes, setSearchRes] = useState<SearchResp | undefined>(undefined);
+  console.log('🚀 ~ App ~ searchRes:', searchRes);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <Masthead></Masthead>
+        <div className='h-36 shadow-md flex justify-center items-center'>
+          <SearchBar setSearchRes={setSearchRes}></SearchBar>
+        </div>
+        {searchRes && (
+          <div className='w-full flex justify-center'>
+            <div className='w-full max-w-[1120px]'>
+              <div className='mt-8'>
+                <p
+                  style={{fontFamily: 'Open Sans'}}
+                  className='font-semibold text-2xl'>
+                  Showing {searchRes.Page} -{' '}
+                  {searchRes.Page * searchRes.PageSize} of{' '}
+                  {searchRes.TotalNumberOfResults}
+                </p>
+              </div>
+              <div>
+                {searchRes.ResultItems.map((item) => (
+                  <SearchResOptions
+                    key={item.DocumentId}
+                    item={item}></SearchResOptions>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </QueryClientProvider>
   );
 }
 
